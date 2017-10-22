06ca1d3544de652a63e96fc2b9f8ffc53583fe56
xomachine <xomachiner@gmail.com>
2017-10-23 00:25:46 +0300

    Code deduplication

--- co_protocol/signature.nim
+++ co_protocol/signature.nim
@@ -9,17 +9,20 @@ const signatureFilePath = "~/.cooperation/signature.key"
 from pipeproto import serialize
 from streams import newStringStream
 
+proc getHash(request: SignedRequest): string =
+  ## Obtains correct hash for given ``request`` using current user key.
+  var unsignedRequest = request
+  unsignedRequest.passhash = readFile(signatureFilePath)
+  let serialized = unsignedRequest.serialize()
+  $secureHash(serialized)
+
 proc sign(request: SignedRequest): SignedRequest =
-  ## Signs the request.
+  ## Signs the ``request`` with current user key.
+  let signature = getHash(request)
   result = request
-  result.passhash = readFile(signatureFilePath)# get it somewere
-  let serialized = result.serialize()
-  let signature = $secureHash(serialized)
   result.passhash = signature
 
 proc checkSignature(request: SignedRequest): bool =
-  ## Checks if request has correct signature.
-  var testRequest = request
-  testRequest.passhash = readFile(signatureFilePath)
-  request.passhash == $secureHash(testRequest.serialize())
+  ## Checks if ``request`` has correct signature for current user.
+  request.passhash == getHash(request)
 
